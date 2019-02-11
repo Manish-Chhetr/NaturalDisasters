17739475ef6e909d5c609a6ee610551b1e5bde69
chaotic-enigma <loopholesameer@gmail.com>
2019-02-11 19:18:03 +0530

    cleaned a bit, removed unwanted

--- seismic_analysis/base_cont.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
-
-plt.figure(figsize=(8, 8))
-m = Basemap(projection='ortho', resolution=None, lat_0=50, lon_0=-100)
-m.bluemarble(scale=0.5);
-
-plt.show()
